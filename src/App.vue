bfa41a6e31351948599b71333b8816fa6cc5277a
elissa <yusyu1108@gmail.com>
2024-05-24 01:48:02 +0800

    feat: 排序

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,14 @@ const searchDebounce = useDebounceFn(() => {
   search();
 }, 1000);
 
+const sortData = (order, name) => {
+  if (order === "asc") {
+    filterUBikeStops.value.sort((a, b) => a[name] - b[name]);
+  } else {
+    filterUBikeStops.value.sort((a, b) => b[name] - a[name]);
+  }
+};
+
 const calculatePage = (data = uBikeStops.value) => {
   filterUBikeStops.value = data;
 };
@@ -73,13 +81,29 @@ const timeFormat = (val) => {
           <th>場站區域</th>
           <th>
             目前可用車輛
-            <i class="fa fa-sort-asc" aria-hidden="true"></i>
-            <i class="fa fa-sort-desc" aria-hidden="true"></i>
+            <i
+              class="fa fa-sort-asc"
+              aria-hidden="true"
+              @click="sortData('asc', 'available_rent_bikes')"
+            ></i>
+            <i
+              class="fa fa-sort-desc"
+              aria-hidden="true"
+              @click="sortData('desc', 'available_rent_bikes')"
+            ></i>
           </th>
           <th>
             總停車格
-            <i class="fa fa-sort-asc" aria-hidden="true"></i>
-            <i class="fa fa-sort-desc" aria-hidden="true"></i>
+            <i
+              class="fa fa-sort-asc"
+              aria-hidden="true"
+              @click="sortData('asc', 'total')"
+            ></i>
+            <i
+              class="fa fa-sort-desc"
+              aria-hidden="true"
+              @click="sortData('desc', 'total')"
+            ></i>
           </th>
           <th>資料更新時間</th>
         </tr>
