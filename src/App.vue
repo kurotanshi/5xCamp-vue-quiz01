5e551604d20b117143523bf9cc6a75ff2c6b0ebc
elissa <yusyu1108@gmail.com>
2024-05-24 01:55:28 +0800

    feat: 分頁

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref, watch } from "vue";
 import { useDebounceFn } from "@vueuse/core";
 
 const uBikeStops = ref([]);
 const filterUBikeStops = ref([]);
 const searchKey = ref("");
+const currentPage = ref(1);
+const totalPages = ref(0);
+const limit = ref(10);
+const visiblePages = computed(() => {
+  const start = Math.max(1, currentPage.value - 5);
+  const end = Math.min(totalPages.value, currentPage.value + 4);
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+});
+const isFirstPage = computed(() => currentPage.value === 1);
+const isLastPage = computed(() => currentPage.value >= totalPages.value);
 
 const search = () => {
   if (searchKey.value === "") {
@@ -28,9 +38,44 @@ const sortData = (order, name) => {
   }
 };
 
-const calculatePage = (data = uBikeStops.value) => {
-  filterUBikeStops.value = data;
+const showMorePage = (page) => {
+  return visiblePages.value.includes(page);
 };
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++;
+    calculatePage();
+  }
+};
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+    calculatePage();
+  }
+};
+const goToFirstPage = () => {
+  currentPage.value = 1;
+  calculatePage();
+};
+const goToLastPage = () => {
+  currentPage.value = totalPages.value;
+  calculatePage();
+};
+const goToPage = (page) => {
+  currentPage.value = page;
+  calculatePage();
+};
+const calculatePage = (data = uBikeStops.value) => {
+  const start = (currentPage.value - 1) * limit.value;
+  const end = start + limit.value;
+  filterUBikeStops.value = data.slice(start, end);
+  totalPages.value = Math.ceil(data.length / limit.value);
+};
+
+watch(limit, () => {
+  // 問題：這裡有一個 BUG 不確定怎麼解比較好，當前頁面在最末頁時，改變每頁筆數，會計算錯誤
+  calculatePage();
+});
 
 // 欄位說明:
 // sno：站點代號、 sna：場站名稱(中文)、 total：場站總停車格、
@@ -121,51 +166,82 @@ const timeFormat = (val) => {
     </table>
 
     <!-- 頁籤 -->
-    <ul class="my-4 flex justify-center">
-      <li class="page-item cursor-pointer">
-        <span class="page-link">第一頁</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">&lt;</span>
-      </li>
-
-      <li class="page-item cursor-pointer active">
-        <span class="page-link">1</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">2</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">3</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">4</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">5</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">6</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">7</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">8</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">9</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">10</span>
-      </li>
-
-      <li class="page-item cursor-pointer">
-        <span class="page-link" href>&gt;</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">最末頁</span>
-      </li>
-    </ul>
+    <div class="flex items-center justify-center space-x-4">
+      <ul class="my-4 flex">
+        <li class="page-item cursor-pointer">
+          <span
+            class="page-link"
+            :class="{
+              'text-gray-400 bg-gray-100 cursor-not-allowed hover:text-gray-400':
+                isFirstPage,
+            }"
+            :disabled="isFirstPage"
+            @click="goToFirstPage"
+            >第一頁</span
+          >
+        </li>
+        <li class="page-item cursor-pointer">
+          <span
+            class="page-link"
+            :class="{
+              'text-gray-400 bg-gray-100 cursor-not-allowed hover:text-gray-400':
+                isFirstPage,
+            }"
+            :disabled="isFirstPage"
+            @click="prevPage"
+            >&lt;</span
+          >
+        </li>
+        <li class="page-item" v-if="!showMorePage(1)">
+          <span class="page-link cursor-not-allowed text-gray-400 border-y-0"
+            >...</span
+          >
+        </li>
+        <li
+          class="page-item cursor-pointer"
+          :class="{ active: currentPage === page }"
+          v-for="page in visiblePages"
+          :key="page"
+        >
+          <span class="page-link" @click="goToPage(page)">{{ page }}</span>
+        </li>
+        <li class="page-item" v-if="!showMorePage(totalPages)">
+          <span class="page-link cursor-not-allowed text-gray-400 border-y-0"
+            >...</span
+          >
+        </li>
+        <li class="page-item cursor-pointer">
+          <span
+            class="page-link"
+            :class="{
+              'text-gray-400 bg-gray-100 cursor-not-allowed hover:text-gray-400':
+                isLastPage,
+            }"
+            :disabled="isLastPage"
+            @click="nextPage"
+            >&gt;</span
+          >
+        </li>
+        <li class="page-item cursor-pointer">
+          <span
+            class="page-link"
+            :class="{
+              'text-gray-400 bg-gray-100 cursor-not-allowed hover:text-gray-400':
+                isLastPage,
+            }"
+            :disabled="isLastPage"
+            @click="goToLastPage"
+            >最末頁</span
+          >
+        </li>
+      </ul>
+      <div>
+        每頁<input
+          class="border w-16 mx-2"
+          type="number"
+          v-model.lazy="limit"
+        />筆
+      </div>
+    </div>
   </div>
 </template>
