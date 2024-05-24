361fe0492ae51b393da1ba92947cbf28d68cf949
jasontsai <jasontsai99@gmail.com>
2024-05-24 17:22:38 +0800

    backup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 const uBikeStops = ref([]);
+const keyword = ref('');
+const sortKey = ref('sno');
+const sortOrder = ref('asc');
+
+const currentPage = ref(1);
+const perPage = ref(5); // 每頁顯示筆數
+const pagemenucount = ref(10); // 頁碼數
 
 // 欄位說明:
 // sno：站點代號、 sna：場站名稱(中文)、 total：場站總停車格、
@@ -19,10 +26,74 @@ const timeFormat = (val) => {
   const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
   return val.replace(pattern, '$1/$2/$3 $4:$5:$6');
 };
-</script>
 
-<template>  
-<!--
+
+// 篩選站點 (搜尋) + 排序 computed
+const queryStops = computed(() => {
+  let stops = uBikeStops.value.filter((item) => {
+    return item.sna.includes(keyword.value);
+  });
+  // 排序
+  if (sortKey.value) {
+    stops = stops.sort((a, b) => {
+      if (sortOrder.value === 'asc') {
+        return a[sortKey.value] - b[sortKey.value];
+      } else {
+        return b[sortKey.value] - a[sortKey.value];
+      }
+    });
+  }
+
+  return stops;
+});
+
+// 關鍵字高亮
+const hightlight = (val) => {
+  console.log('hightlight');
+  if (!keyword.value) {
+    return val;
+  }
+  return val.replace(keyword.value, `<span style="color:red">${keyword.value}</span>`);
+};
+
+// 排序
+const sort = (key) => {
+  if (sortKey.value === key) {
+    sortOrder.value = sortOrder.value === 'asc' ? 'desc' : 'asc';
+  } else {
+    sortKey.value = key;
+    sortOrder.value = 'asc';
+  }
+};
+
+// 分頁
+const changePage = (page) => {
+  currentPage.value = page;
+  console.log(page);
+};
+
+// 第一頁
+const firstPage = () => {
+  currentPage.value = 1;
+};
+
+// 最後一頁
+const lastPage = () => {
+  currentPage.value = (queryStops.value.length / perPage.value) + 1;
+  //currentPage.value = Math.ceil(queryStops.value.length / perPage.value);
+};
+
+const prevPage = () => {
+  currentPage.value = currentPage.value > 1 ? currentPage.value - 1 : 1;
+};
+
+const nextPage = () => {
+  currentPage.value = currentPage.value + 1;
+}
+
+</script>
+<template>
+  <!--
 練習： YouBike 臺北市公共自行車即時資訊，實作以下功能
 1. 站點名稱搜尋
 2. 目前可用車輛 / 總停車格 的排序功能
@@ -30,31 +101,32 @@ const timeFormat = (val) => {
 -->
   <div class="my-4">
     <p class="my-4 pl-2">
-      站點名稱搜尋: 
-      <input type="text" class="border w-60 p-1 ml-2">
+      站點名稱搜尋:
+      <input type="text" class="border w-60 p-1 ml-2" v-model="keyword">
     </p>
-    
+
     <table class="table table-striped">
       <thead>
         <tr>
-          <th class="w-12">#</th>
+          <th class="w-12 cursor-pointer" @click="sort('sno')">#<i class="fa" v-if="sortKey === 'sno'"
+              :class="sortOrder === 'asc' ? 'fa-sort-asc' : 'fa-sort-desc'" aria-hidden="true"></i></th>
           <th>場站名稱</th>
           <th>場站區域</th>
-          <th>目前可用車輛
-            <i class="fa fa-sort-asc" aria-hidden="true"></i>
-            <i class="fa fa-sort-desc" aria-hidden="true"></i>
+          <th class="cursor-pointer" @click="sort('available_rent_bikes')">目前可用車輛
+            <i class="fa" v-if="sortKey === 'available_rent_bikes'"
+              :class="sortOrder === 'asc' ? 'fa-sort-asc' : 'fa-sort-desc'" aria-hidden="true"></i>
           </th>
-          <th>總停車格
-            <i class="fa fa-sort-asc" aria-hidden="true"></i>
-            <i class="fa fa-sort-desc" aria-hidden="true"></i>
+          <th class="cursor-pointer" @click="sort('total')">總停車格
+            <i class="fa" v-if="sortKey === 'total'" :class="sortOrder === 'asc' ? 'fa-sort-asc' : 'fa-sort-desc'"
+              aria-hidden="true"></i>
           </th>
-          <th>資料更新時間</th>          
+          <th>資料更新時間</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(s, idx) in uBikeStops" :key="s.sno">
-          <td>{{ idx +1 }}</td>
-          <td>{{ s.sna }}</td>
+        <tr v-for="(s, idx) in queryStops" :key="s.sno">
+          <td>{{ idx + 1 }}</td>
+          <td v-html="hightlight(s.sna)"></td>
           <td>{{ s.sarea }}</td>
           <td>{{ s.available_rent_bikes }}</td>
           <td>{{ s.total }}</td>
@@ -62,51 +134,25 @@ const timeFormat = (val) => {
         </tr>
       </tbody>
     </table>
-    
+
     <!-- 頁籤 -->
     <ul class="my-4 flex justify-center">
-      <li class="page-item cursor-pointer">
+      <li class="page-item cursor-pointer" @click="firstPage">
         <span class="page-link">第一頁</span>
       </li>
-      <li class="page-item cursor-pointer">
+      <li class="page-item cursor-pointer" @click="prevPage">
         <span class="page-link">&lt;</span>
       </li>
 
-      <li class="page-item cursor-pointer active">
-        <span class="page-link">1</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">2</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">3</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">4</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">5</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">6</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">7</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">8</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">9</span>
-      </li>
-      <li class="page-item cursor-pointer">
-        <span class="page-link">10</span>
+      <li class="page-item cursor-pointer" v-for="i in pagemenucount" :key="i"
+        :class="currentPage === i ? 'active' : ''" @click="changePage(i)">
+        <span class="page-link">{{ i }}</span>
       </li>
 
-      <li class="page-item cursor-pointer">
+      <li class="page-item cursor-pointer" @click="nextPage">
         <span class="page-link" href>&gt;</span>
-      </li>      
-      <li class="page-item cursor-pointer">
+      </li>
+      <li class="page-item cursor-pointer" @click="lastPage">
         <span class="page-link">最末頁</span>
       </li>
     </ul>
