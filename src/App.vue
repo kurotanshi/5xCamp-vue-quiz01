e8f85e21906b8deb4179513c968d0191f298c4b1
Kuro Hsu <kurotanshi@gmail.com>
2024-09-06 19:00:49 +0800

    update: 1. vue 版本更新 2. API 換成新北市 ubike

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
 import { ref } from 'vue';
 const uBikeStops = ref([]);
 
-// 欄位說明:
+// 資料來源: https://data.ntpc.gov.tw/openapi/swagger-ui/index.html?configUrl=%2Fapi%2Fv1%2Fopenapi%2Fswagger%2Fconfig&urls.primaryName=%E6%96%B0%E5%8C%97%E5%B8%82%E6%94%BF%E5%BA%9C%E4%BA%A4%E9%80%9A%E5%B1%80(94)#/JSON/get_010e5b15_3823_4b20_b401_b1cf000550c5_json
+
+// 欄位說明: 
 // sno：站點代號、 sna：場站名稱(中文)、 total：場站總停車格、
 // available_rent_bikes：場站目前可用車輛數量、 
 // sarea：場站區域(中文)、 mday：資料更新時間、
 // lat：緯度、 lng：經度、 ar：地(中文)、 sareaen：場站區域(英文)、
 // snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
-fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
+
+// page: 頁碼, size: 每頁筆數, 全部 349 筆.
+fetch('https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?page=1&size=999')
   .then(res => res.text())
   .then(data => {
     uBikeStops.value = JSON.parse(data);
@@ -23,16 +27,27 @@ const timeFormat = (val) => {
 
 <template>  
 <!--
-練習： YouBike 臺北市公共自行車即時資訊，實作以下功能
-1. 站點名稱搜尋
-2. 目前可用車輛 / 總停車格 的排序功能
-3. 分頁功能 (Optional)
+練習： YouBike 新北市公共自行車即時資訊，實作以下功能
+  1. 站點名稱搜尋
+  2. 目前可用車輛 / 總停車格 的排序功能
+  3. 分頁功能, 已知總筆數為 349 筆
 -->
-  <div class="my-4">
-    <p class="my-4 pl-2">
-      站點名稱搜尋: 
-      <input type="text" class="border w-60 p-1 ml-2">
-    </p>
+
+  <div class="">
+    <div class="grid grid-cols-2 my-4 px-4 w-full mx-auto">
+      <div class="pl-2">
+        目前頁面的站點名稱搜尋: <input type="text" class="border w-60 p-1 ml-2">
+      </div>
+      <div class="pl-2">
+        每頁顯示筆數: 
+        <select class="border w-20 p-1 ml-2">
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+        </select>
+      </div>
+    </div>
+
     
     <table class="table table-striped">
       <thead>
@@ -56,8 +71,8 @@ const timeFormat = (val) => {
           <td>{{ idx +1 }}</td>
           <td>{{ s.sna }}</td>
           <td>{{ s.sarea }}</td>
-          <td>{{ s.available_rent_bikes }}</td>
-          <td>{{ s.total }}</td>
+          <td>{{ s.sbi }}</td>
+          <td>{{ s.tot }}</td>
           <td>{{ timeFormat(s.mday) }}</td>
         </tr>
       </tbody>
